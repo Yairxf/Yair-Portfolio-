3d7059adaf8e48dca8da8e050c85467021c4b093
Yair8798 <yair8798@gmail.com>
2022-11-11 13:07:11 +0200

    Form Validation

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,13 +1,13 @@
 function sendMessage() {
     const request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/1019950776711397386/GV8SpnL0gpfBWqk1p026l9MY6R81F6lZpXiyOkwCaISC_j0IX_YtonlLhhPD4SlW_dnC");
+    request.open("POST", "https://discord.com/api/webhooks/1040582936250036316/uCHN1JJpJyEV5ntn6UOMec04SBmQuXn1hvzgSqy4G-b3R3wdP77Q8K30tLSsfEDJa9Lh");
 
     request.setRequestHeader('Content-type', 'application/json');
 
     const params = {
         username: "Yair Contact Form Website",
         avatar_url: "",
-        content: `${document.getElementById("tel").value} + ${document.getElementById("name").value} + ${document.getElementById("email").value} + ${document.getElementById("subject").value}`
+        content: `${document.getElementById("tel").value} + ${document.getElementById("name").value} + ${document.getElementById("email").value} + ${document.getElementById("text").value}`
 
     }
 
